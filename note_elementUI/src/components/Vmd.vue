28a67c65d40e603903fd3693881619329e015f33
u19900101 <815000342@qq.com>
2021-11-22 23:28:40 +0800

    d 自定义工具栏和主题  无法设置初始编辑器主题

--- note_elementUI/src/components/Vmd.vue
+++ note_elementUI/src/components/Vmd.vue
@@ -16,8 +16,16 @@
             let vm = this
             this.contentEditor = new Vditor("vditor", {
                 height: 670,
+                toolbar: [
+                    'emoji', 'br', 'bold', '|', 'line','code-theme' , 'content-theme'],
                 toolbarConfig: {
-                    pin: true
+                    pin: true, //pin	是否固定工具栏	false
+                    // hide: true, //是否隐藏工具栏	false
+                },
+                counter: {enable: true, type: 'text'}, //'markdown', 'text'
+                preview: {
+                    theme:
+                        {current : "dark"} //, dark light  const v = this.contentEditor  v.setTheme("dark","dark","dark")
                 },
                 cache: {
                     enable: false
@@ -26,9 +34,9 @@
                     this.contentEditor.setValue(this.$store.state.currentNote.content)
                 },
                 input(content) {
+                    vm.$store.state.currentNote.content = content
                     // 没有标题,默认提取前10个字符为标题'
                     let title = content.substring(0, 10)
-                    console.log('kkk',title)
                     let note = {
                         id: vm.$store.state.currentNote.id,
                         content: content,
@@ -41,9 +49,9 @@
                         title = content.match(re)[0].substring(2)
                         note.title = title.replace("\n\n", "")
                     }
-                     vm.https.updateNote(note).then(({data}) => {
-                         console.log("修改数据库成功", data);
-                     })
+                    vm.https.updateNote(note).then(({data}) => {
+                        console.log("修改数据库成功", data);
+                    })
                 }
             })
         },
